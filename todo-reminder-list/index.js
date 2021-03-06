99302113779abc5521ad90925d1af92407ce3f32
Kevin Chan <kevinchan128@msn.com>
2021-03-06 13:56:26 -0800

    Storing user data part 2
    Successfully stored each user string data as each task but now need to figure out how to do it for checkboxes.

--- todo-reminder-list/index.js
+++ todo-reminder-list/index.js
@@ -1,45 +1,68 @@
 var backendList = [];
 let counter = 0;
-// window.onload = function () {
-//     // var ul = document.getElementById('list');
-//     // var li = document.createElement('li');
-//     // for(var i = 0; i < backendList.length; i++) {
-//     //     li.appendChild(document.createTextNode(backendList[i]));
-//     //     ul.appendChild(li);
-//     // }
-//     // chrome.storage.local.get(['item'], function(result) {
-//     //     console.log(result.key);
-//     // })
-// }
 
-
+//when add button is clicked
 document.getElementById('addButton').onclick = function() {
     var ul = document.getElementById('list');
     var li = document.createElement('li');
+    li.class = 'liItems';
     var text = document.getElementById('textInput').value;
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.value = 'value';
+    checkbox.class = 'checkboxes';
+    checkbox.htmlFor = 'liItems';
+    li.appendChild(checkbox);
     li.appendChild(document.createTextNode(text));
     ul.appendChild(li);
-    // backendList.push(text);
-    chrome.storage.sync.set({item: text}, function() {
-        console.log('here is the value:' + text );
+    backendList.push(text);
+    chrome.storage.sync.set({list: backendList}, function() {
+        console.log('here is the value: ' + list );
+        // console.log('backend: ' + backendList[0]);
     });
-    chrome.storage.sync.get(['item'], function(result) {
-        console.log('here is the get value:' + result.item);
-    });
-    counter++;
     document.getElementById("textInput").value = "";
 }
 
-document.getElementById('list').onload = function() {
+//happens when the page loads
+window.onload = function() {
     var ul = document.getElementById('list');
-    var li = document.createElement('li');
-    var text = '';
-    chrome.storage.sync.get(['item'], function(result) {
-        text = result.item;
-        console.log('text: ' + text);
+    var text = [];
+    chrome.storage.sync.get({list: []}, function() {
+        for(var i = 0; i < list.length; i++) {
+            backendList[i] = list[i];
+        }
+    })
+    chrome.storage.sync.get({
+        list: []
+    }, function(result) {
+        text = result.list;
+        for(var i = 0; i < text.length; i++) {
+            var li = document.createElement('li');
+            li.id = 'liItem' + i.toString();
+            var checkbox = document.createElement('input');
+            checkbox.id = 'checkbox' + i.toString();
+            checkbox.class = 'checkboxClass';
+            console.log('checkboxid: ' + checkbox.id);
+            checkbox.type = 'checkbox';
+            checkbox.value = 'value';
+            checkbox.class = 'checkboxes';
+            checkbox.htmlFor = 'liItems';
+            checkbox.onchange = function () {
+                if(document.getElementById('checkbox' + i.toString()).checked) {
+                    console.log('test');
+                    document.getElementById('checkbox' + i.toString()).style.textDecoration = 'line-through';
+                }
+            };
+            li.appendChild(checkbox);
+            backendList[i] = text[i];
+            console.log('backendList: ' + backendList[i]);
+            li.appendChild(document.createTextNode(backendList[i]));
+            ul.appendChild(li);
+        }
+        // console.log('backend array: ' + backendList);
     });
-    for(var i = 0; i < backendList.length; i++) {
-        li.appendChild(document.createTextNode(text));
-        ul.appendChild(li);
-    }
+}
+
+document.getElementsByClassName('checkBoxClass').checked = function () {
+    
 }
