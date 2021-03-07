641d00fa268adb967758101b3cd86e9ea924e876
Kevin Chan <kevinchan128@msn.com>
2021-03-06 23:52:02 -0800

    checkbox: line through text
    still have not figured out how to get text crossed out after checking checkboxes.

--- todo-reminder-list/index.js
+++ todo-reminder-list/index.js
@@ -2,6 +2,8 @@ var backendList = [];
 var checkboxList = [];
 var counter = 0;
 
+var itemList = [];
+
 //when add button is clicked
 document.getElementById('addButton').onclick = function() {
     var ul = document.getElementById('list');
@@ -13,20 +15,19 @@ document.getElementById('addButton').onclick = function() {
     });
     var text = document.getElementById('textInput').value;
     var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.class = 'checkboxes';
+    checkbox.setAttribute('type', 'checkbox');
     checkbox.setAttribute('for', 'liItem' + counter.toString());
     checkbox.setAttribute('id','checkboxes' + counter.toString());
-    // checkbox.setAttribute('checked', '');
+    // checkbox.setAttribute('checked','false');
     li.appendChild(checkbox);
     li.appendChild(document.createTextNode(text));
     ul.appendChild(li);
+    itemList.push(li);
     backendList.push(text);
     checkboxList.push(checkbox);
-    console.log(checkboxList);
     chrome.storage.sync.set({list: backendList, checkList: checkboxList}, function() {
         // console.log('here is the value: ' + list );
-        console.log('here is the checkboxlist: ' + checkboxList);
+        // console.log('here is the checkboxlist: ' + JSON.stringify(checkboxList[0]));
         // console.log('backend: ' + backendList[0]);
     });
     document.getElementById("textInput").value = "";
@@ -43,65 +44,55 @@ window.onload = function() {
     var ul = document.getElementById('list');
     var text = [];
     var checklist = [];
-    chrome.storage.sync.get({list: []}, function() {
+    chrome.storage.sync.get({list: [], checkList: []}, function() {
         for(var i = 0; i < list.length; i++) {
             backendList[i] = list[i];
+            checkboxList[i] = checkList[i];
         }
+        console.log('get checkboxList: ' + checkboxList[0]);
     })
     chrome.storage.sync.get({
         list: [],
-        checkboxList: []
+        checkList: []
     }, function(result) {
         text = result.list;
-        checklist = result.checkboxList;
+        checklist = result.checkList;
         for(var i = 0; i < text.length; i++) {
             var li = document.createElement('li');
             li.id = 'liItem' + i.toString();
             var checkbox = document.createElement('input');
-            checkbox.id = 'checkbox' + i.toString();
-            checkbox.class = 'checkboxClass';
+            checkbox.setAttribute('id', 'checkboxes' + i.toString());
             console.log('checkboxid: ' + checkbox.id);
-            checkbox.type = 'checkbox';
-            checkbox.class = 'checkboxes';
-            checkbox.htmlFor = 'liItem' + i.toString();
+            checkbox.setAttribute('type', 'checkbox');
+            checkbox.setAttribute('for', 'liItem' + i.toString());
+            // checkbox.setAttribute('checked', '');
             li.appendChild(checkbox);
             backendList[i] = text[i];
-            checkboxList[i] =checklist[i];
+            //checkbox isnt saving anything
+            // checkboxList[i] = checkList[i];
             // console.log('backendList: ' + backendList[i]);
             // console.log('checkboxList: ' + checkboxList[i]);
             li.appendChild(document.createTextNode(backendList[i]));
             ul.appendChild(li);
-            // counter++;
         }
         console.log(checkboxList);
         // console.log('backend array: ' + backendList);
     });
 }
-//when each checkbox is checked
-// document.getElementsByClassName('checkboxes').onchange = function () {
-//     console.log('afasdfas');
-//     for(var i = 0; i < checkboxList.length; i++) {
-//         if(document.getElementById('liItem' + i.toString()).checked) {
-//             document.getElementById('liItem' + i.toString()).style.textDecoration = 'line-through';
-//         } else {
-//             document.getElementById('liItem' + i.toString()).style.textDecoration = 'none';
+
+// if(document.getElementById('checkboxes0').checked) {
+//     document.getElementById('checkboxes0').onchange = function() {
+//         document.getElementById('checkboxes0').style.textDecoration = 'line-through';
+//         if(document.getElementById('checkboxes0').checked === false) {
+//             document.getElementById('checkboxes0').style.textDecoration = 'none';
 //         }
 //     }
 // }
 
-if(document.getElementById('checkboxes0')) {
-    document.getElementById('checkboxes0').onchange = function() {
-        document.getElementById('checkboxes0').style.textDecoration = 'line-through';
-        if(document.getElementById('checkboxes0').checked === false) {
-            document.getElementById('checkboxes0').style.textDecoration = 'none';
-        }
+//for when checkbox is checked, single test case only
+document.getElementById('testBox').onchange = function() {
+    document.getElementById('testString').style.textDecoration = 'line-through';
+    if(document.getElementById('testBox').checked === false) {
+        document.getElementById('testString').style.textDecoration = 'none';
     }
 }
-
-//for when checkbox is checked, single test case only
-// document.getElementById('testBox').onchange = function() {
-//     document.getElementById('testString').style.textDecoration = 'line-through';
-//     if(document.getElementById('testBox').checked === false) {
-//         document.getElementById('testString').style.textDecoration = 'none';
-//     }
-// }
